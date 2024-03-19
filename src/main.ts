8ef5fa5eee74384063a918320bf9709a31beb094
Diego Rojas <devup2332@gmail.com>
2024-03-19 01:27:02 -0500

    master: Edited eslint config file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { PORT } from './config/environments';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { PORT } from "./config/environments";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
